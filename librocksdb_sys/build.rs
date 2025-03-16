8b54b47ae7d9f345e569b5d61c5bfa2b282de1c2
homeffjy <homeffjy@gmail.com>
2025-03-16 05:53:01 +0000

    set flag

--- librocksdb_sys/build.rs
+++ librocksdb_sys/build.rs
@@ -144,6 +144,7 @@ fn build_rocksdb() -> Build {
     }
     if cfg!(feature = "aws") {
         cfg.define("WITH_AWS", "ON");
+        cfg.define("WITH_TITAN_MODS", "ON");
         println!("cargo:rustc-link-search=native={}", env::var("DEP_AWSSDK_ROOT").unwrap_or_else(|_| "/usr/local/lib".to_string()));
         println!("cargo:rustc-link-lib=aws-cpp-sdk-core");
         println!("cargo:rustc-link-lib=aws-cpp-sdk-s3");
